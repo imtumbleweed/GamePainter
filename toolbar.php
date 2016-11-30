3201b3e47ca9c6665e5b0c218d6aa66ac08f28e9
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2016-11-30 04:42:48 -0500

    Added camera icon to the view

--- toolbar.php
+++ toolbar.php
@@ -1,5 +1,5 @@
 <style>
-    #Tools { background: #535353 url('toolboxbody.png') repeat-y; width: 72px; height: 200px; position: absolute; top: 80px; left: calc(50% + 200px);  }
+    #Tools { background: #535353 url('toolboxbody.png') repeat-y; width: 72px; height: 200px; position: absolute; top: 80px; left: calc(50% + 200px); }
     #ToolsHeader { position: relative; width: 72px; height: 22px; background: #535353 url('toolbox-top.png') no-repeat; }
     .ToolIcon { width: 33px; height: 26px; background: url("ic-empty.png") no-repeat; display: inline-block; margin:0; }
     .Toolpad { margin-left: 3px; }
@@ -7,6 +7,7 @@
     .ToolIC-Empty { background: url("ic-empty.png") no-repeat; }
     .ToolIcon:hover { background: url("ic-empty.png") no-repeat; cursor: pointer; }
     .ToolIcon.Selected { background: url("ic-selected.png") no-repeat; }
+    .CameraIcon { background: transparent url('camera.png') repeat-y; width: 24px; height: 24px; position: absolute; top: 70px; left: calc(50% - 350px); }
 </style>
 <script>$(document).ready(function() {
         // Animate toolbar to its last position
@@ -36,3 +37,4 @@
     <div id = "Tool3" class = "ToolIcon Toolpad ToolIC-Empty Selected"><img src = "boxtool.png" alt = "Box Tool"/></div><div id = "Tool4" class = "ToolIcon ToolIC-Empty"><img src = "circletool.png" alt = "Circle Tool"/></div>
     <div id = "Tool5" class = "ToolIcon Toolpad ToolIC-Empty"><img src = "erasertool.png" alt = "Box Tool"/></div><div id = "Tool4" class = "ToolIcon ToolIC-Empty"></div>
 </div>
+<div class = "CameraIcon"></div>
