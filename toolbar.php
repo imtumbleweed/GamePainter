6a5985fc3211f6edf9c9cf6dfafe35174060d938
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2016-11-30 05:45:45 -0500

    Added secondary toolbar, added box shadow to toolbars.

--- toolbar.php
+++ toolbar.php
@@ -1,5 +1,5 @@
 <style>
-    #Tools { background: #535353 url('toolboxbody.png') repeat-y; width: 72px; height: 200px; position: absolute; top: 80px; left: calc(50% + 200px); }
+    #Tools { box-shadow: 0 0 40px #222; background: #535353 url('toolboxbody.png') repeat-y; width: 72px; height: 200px; position: absolute; top: 80px; left: calc(50% + 200px); }
     #ToolsHeader { position: relative; width: 72px; height: 22px; background: #535353 url('toolbox-top.png') no-repeat; }
     .ToolIcon { width: 33px; height: 26px; background: url("ic-empty.png") no-repeat; display: inline-block; margin:0; }
     .Toolpad { margin-left: 3px; }
@@ -8,8 +8,13 @@
     .ToolIcon:hover { background: url("ic-empty.png") no-repeat; cursor: pointer; }
     .ToolIcon.Selected { background: url("ic-selected.png") no-repeat; }
     .CameraIcon { background: transparent url('camera.png') repeat-y; width: 24px; height: 24px; position: absolute; top: 70px; left: calc(50% - 350px); }
+    #SecondaryToolbar { box-shadow: 0 0 40px #222;  background: #535353 url('SecondaryToolbar.png'); width: 100px; height: 50px; position: absolute; top: 32px; left: calc(50% + 140px); }
+    .STPlaceholder { position: relative; margin; 2px; display: inline-block; width: 33px; height; 26px; background: url("SecondaryToolbarSelected.png") no-repeat; }
+    .STAdjuster { position: absolute; top: 16px; left: 18px; }
 </style>
 <script>$(document).ready(function() {
+        //
+        document.getElementsByTagName('img').ondragstart = function() { return false; };
         // Animate toolbar to its last position
         if (localStorage) {
             var x = parseInt(localStorage.getItem("toolbarx"));
@@ -24,6 +29,7 @@
         $("#Tool3").on("click", function(){ toolbox.currentToolID = toolbox.BOX_TOOL; console.log("Selected Box Tool.");});
         $("#Tool4").on("click", function(){ toolbox.currentToolID = toolbox.CIRCLE_TOOL; console.log("Selected Circle Tool.");});
         $("#Tool5").on("click", function(){ toolbox.currentToolID = toolbox.ERASER_TOOL; console.log("Selected Eraser Tool.");});
+        $("#Tool6").on("click", function(){ toolbox.currentToolID = toolbox.PLAYER_TOOL; console.log("Player Tool.");});
 
         // Make clickable tool icons
         $('.ToolIcon').on("click", function() {
@@ -35,6 +41,17 @@
     <div style = "height: 6px;"></div>
     <div id = "Tool1" class = "ToolIcon Toolpad"><img src = "ic1a.png" alt = "Select Objects"/></div><div id = "Tool2" class = "ToolIcon"><img src = "ic2a.png" alt = "Move World"/></div>
     <div id = "Tool3" class = "ToolIcon Toolpad ToolIC-Empty Selected"><img src = "boxtool.png" alt = "Box Tool"/></div><div id = "Tool4" class = "ToolIcon ToolIC-Empty"><img src = "circletool.png" alt = "Circle Tool"/></div>
-    <div id = "Tool5" class = "ToolIcon Toolpad ToolIC-Empty"><img src = "erasertool.png" alt = "Box Tool"/></div><div id = "Tool4" class = "ToolIcon ToolIC-Empty"></div>
+    <div id = "Tool5" class = "ToolIcon Toolpad ToolIC-Empty"><img src = "erasertool.png" alt = "Box Tool"/></div><div id = "Tool6" class = "ToolIcon ToolIC-Empty"></div>
+    <div id = "Tool7" class = "ToolIcon Toolpad ToolIC-Empty"><img src = "playericon.png" alt = "Box Tool"/></div><div id = "Tool8" class = "ToolIcon ToolIC-Empty"></div>
+</div>
+<div id = "SecondaryToolbar">
+    <div class = "STAdjuster">
+        <div class = "STPlaceholder">
+            <img src = "playericon.png" alt = ""/>
+        </div>
+        <div class = "STPlaceholder">
+            <img src = "controllericon.png" alt = ""/>
+        </div>
+    </div>
 </div>
 <div class = "CameraIcon"></div>
