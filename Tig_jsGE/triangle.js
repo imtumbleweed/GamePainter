0336c3e64f17fdfbfa28a4058a92d9f77031f0a7
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2016-12-04 16:32:37 -0500

    Reset triangle when no parameters are passed in constructor

--- Tig_jsGE/triangle.js
+++ Tig_jsGE/triangle.js
@@ -2,9 +2,26 @@ class Triangle {
     constructor(ax0,ay0,avecx,avecy,
                 bx0,by0,bvecx,bvecy,
                 cx0,cy0,cvecx,cvecy) {
+
+        if (arguments.length == 0) {
+            this.ax0 = 0;
+            this.ay0 = 0;
+            this.avecx0 = 0;
+            this.avecy0 = 0;
+            this.bx0 = 0;
+            this.by0 = 0;
+            this.bvecx0 = 0;
+            this.bvecy0 = 0;
+            this.cx0 = 0;
+            this.cy0 = 0;
+            this.cvecx0 = 0;
+            this.cvecy0 = 0;
+        }
+
         this.A = new Segment(ax0,ay0,avecx,avecy);
         this.B = new Segment(bx0,by0,bvecx,bvecy);
         this.C = new Segment(cx0,cy0,cvecx,cvecy);
+
         this.color = "#fff";
         this.draw = () => {
             this.A.draw(1, this.color);
