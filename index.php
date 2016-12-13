ca719b55b53b81abbaa38a34aa9ae938f3f3a94e
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2016-12-13 06:26:59 -0500

    Removed default favicon

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
 <head>
     <title>Game Painter</title>
-    <link rel = "icon" href = "" /> <?php /* http://www.tigrisgames.com/property_stealth/games/gemini/favicon.gif */ ?>
+    <link rel = "icon" href = "" />
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
     <meta name = "viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name = "language" content = "English" />
