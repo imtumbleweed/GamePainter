954da4e7a8d8e4033648e3bc850b3104c86dd39e
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2016-12-13 11:32:40 -0500

    Removed line drawings on rain object while dragging

--- Tig_jsGE/toolbox.js
+++ Tig_jsGE/toolbox.js
@@ -259,15 +259,15 @@ class Toolbox {
                         if (this.pressed) {
                             if (RainArea[i].attachedToMouse) {
                                 var seg = new Rectangle(RainArea[i].drag_x,RainArea[i].drag_y,10,10);
-                                seg.draw("red",true,true);
+                                //seg.draw("red",true,true);
                                 var arb_w = RainArea[i].dragLine.x - Mouse.x;
                                 var arb_h = RainArea[i].dragLine.y - Mouse.y;
                                 RainArea[i].ghostcloud.x = RainArea[i].drag_x - arb_w;
                                 RainArea[i].ghostcloud.y = RainArea[i].drag_y - arb_h;
-                                RainArea[i].ghostcloud.draw(1, "red");
+                                //RainArea[i].ghostcloud.draw(1, "red");
                                 RainArea[i].cloud.x = -grid.x + RainArea[i].ghostcloud.x;
                                 RainArea[i].cloud.y = -grid.y + RainArea[i].ghostcloud.y;
-                                RainArea[i].dragLine.draw(2, "teal");
+                                //RainArea[i].dragLine.draw(2, "teal");
 
                                 //RainArea[i].cloud.x = RainArea[i].drag_x - RainArea[i].bg.x + Mouse.x;
                                 //RainArea[i].y = RainArea[i].drag_y;
